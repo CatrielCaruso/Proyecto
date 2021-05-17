3b6e2b4ecf372979e548ef2248d77bfe4347881f
catriel <catrielcaruso@gmail.com>
2021-05-17 15:25:07 -0300

    Avances

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import{FormsModule} from '@angular/forms';
+
+
+
+//Modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
 
 import { HeaderComponent } from '../shared/header/header.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -21,6 +28,6 @@ import { SharedModule } from '../shared/shared.module';
     Grafica1Component,
     PagesComponent,
   ],
-  imports: [CommonModule, SharedModule, RouterModule],
+  imports: [CommonModule, SharedModule, RouterModule,FormsModule,ComponentsModule],
 })
 export class PagesModule {}
--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
       { path: '', component: DashboardComponent },
       { path: 'progress', component: ProgressComponent },
       { path: 'grafica1', component: Grafica1Component },
-    //   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    
     ],
   },
 ];
