74ca5eadefa4725627fcc6ee293efcb2e0a2c372
catriel <catrielcaruso@gmail.com>
2021-05-22 18:28:39 -0300

    Avances

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -13,13 +13,24 @@ export class SidebarService {
           titulo: 'Dashboard',
           url: '/',
         },
+
+        {
+          titulo: 'Gráfica',
+          url: 'grafica1',
+        },
         {
           titulo: 'ProgressBar',
           url: 'progress',
         },
+        
+
         {
-          titulo: 'Gráfica',
-          url: 'grafica1',
+          titulo: 'Promesas',
+          url: 'promesas',
+        },
+        {
+          titulo: 'Rxjs',
+          url: 'rxjs',
         },
       ],
     },
